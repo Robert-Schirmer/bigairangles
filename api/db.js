4652e6bc3c469e799a3518d08c9225b36fcac8bc
Robert Schirmer <supremecookbot@gmail.com>
2021-12-29 12:58:13 -0500

    Connect through proxy to sql instancee

--- api/db.js
+++ api/db.js
@@ -3,7 +3,8 @@ import * as mysql from "mysql";
 const pool = mysql.createPool({
   multipleStatements: true,
   connectionLimit: 10,
-  host: process.env.DB_HOST,
+  host: '127.0.0.1',
+  port: '3306',
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
--- pages/api/admin/images/add.js
+++ pages/api/admin/images/add.js
@@ -28,6 +28,7 @@ export default async function handler(req, res) {
         return res.status(400).json(`Unable to parse meta data`);
       }
       else {
+        console.log(error);
         return res.status(500).json(`ERROR`);
       }
     }
