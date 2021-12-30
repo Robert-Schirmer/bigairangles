03cbc2e5ae7a8a0d3f8b262df756d9aa09ca529a
Robert Schirmer <supremecookbot@gmail.com>
2021-12-29 21:03:22 -0500

    Connect to host.docker.internal

--- api/db.js
+++ api/db.js
@@ -3,7 +3,7 @@ import * as mysql from "mysql";
 const pool = mysql.createPool({
   multipleStatements: true,
   connectionLimit: 10,
-  host: '127.0.0.1',
+  host: process.env.DB_HOST,
   port: '3306',
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
